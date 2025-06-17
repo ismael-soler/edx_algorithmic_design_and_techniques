4b5467330513e6757724f89fb8a0581f0e4787f1
ismael-soler <ismaelsolerferreira@gmail.com>
2025-06-17 14:38:31 -0300

    Maximum Advertisement Revenue exercies

--- week_3_greedy_algorithms/DotProduct.java
+++ week_3_greedy_algorithms/DotProduct.java
@@ -2,19 +2,12 @@ import java.util.*;
 
 public class DotProduct {
     private static long maxDotProduct(int[] a, int[] b) {
-        
-
-
-
-
-
-
-
-
+        Arrays.sort(a);
+        Arrays.sort(b);
 
         long result = 0;
         for (int i = 0; i < a.length; i++) {
-            result += a[i] * b[i];
+            result += (long)a[i] * b[i];
         }
         return result;
     }
